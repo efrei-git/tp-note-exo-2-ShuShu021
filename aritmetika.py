0e367fba936d4490ffab0d7a92a66fbc8238ce1f
Shvi021 <yohan.lancri@efrei.net>
2025-03-18 12:52:14 +0100

    Intergration: gcd

--- aritmetika.py
+++ aritmetika.py
@@ -1,14 +1,13 @@
 from functions import *
-
 def menu():
     print("\n==== Menu ====")
     print("1. Factorial")
     print("2. Prime Check")
-    print("3. Exit")
-
+    print("3. GCD")
+    print("4. Exit")
 while True:
     menu()
-    choice = input("Enter your choice (1-3): ")
+    choice = input("Enter your choice (1-4): ")
     if choice == '1':
         num = int(input("Enter a number: "))
         fact = factorial(num)
@@ -20,8 +19,12 @@ while True:
         else:
             print(num, "is not a prime number.")
     elif choice == '3':
+        a = int(input("Enter the first number: "))
+        b = int(input("Enter the second number: "))
+        gcd_value = gcd(a, b)
+        print("GCD of", a, "and", b, "is", gcd_value)
+    elif choice == '4':
         print("Exiting the program...")
         break
     else:
         print("Invalid choice. Please try again.")
-
